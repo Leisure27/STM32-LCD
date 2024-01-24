622a571a8947ff98bc3aa08c18bdbfdd0fe04491
MengYang <1973688584@qq.com>
2024-01-24 20:14:54 +0800

    字符显示完成

--- 1.ST7735/Demo/3.LCD/LCD.c
+++ 1.ST7735/Demo/3.LCD/LCD.c
@@ -1,28 +1,38 @@
 #include "ST7735.h"
 
-
 extern TIM_HandleTypeDef htim4;
 char str1[64] = {0};
 
 void init_LCD(void)
 {
     ST7735_Init();
+    ST7735_SetRotation(3);
     ST7735_FillScreen(ST7735_BLACK);
     HAL_TIM_PWM_Start(&htim4, TIM_CHANNEL_2);
-    TIM4->CCR2 = 500; // PWM mode2，值越大屏幕越亮，最大值1000-1，使用手机拍摄时，屏幕亮度设为50
+    TIM4->CCR2 = 300; // PWM mode2，值越大屏幕越亮，最大值1000-1，使用手机拍摄时，屏幕亮度设为50
 }
 
 void LCD_test(void)
 {
     static float i = 0.0f;
     snprintf(str1, 23, "i = %.1f", i);
-    ST7735_WriteString(0, 0, "FOC", Font_11x18, ST7735_RED, ST7735_BLACK);
-    // ST7735_WriteString(40, 0, "MODE1", Font_11x18, ST7735_RED, ST7735_BLACK);
-    // ST7735_WriteString(0, 18, "U=5.68V", Font_11x18, ST7735_GREEN, ST7735_BLACK);
-    // ST7735_WriteString(0, 38, "V=6.23V", Font_11x18, ST7735_BLUE, ST7735_BLACK);
-    // ST7735_WriteString(0, 60, "W=3.26V", Font_11x18, ST7735_YELLOW, ST7735_BLACK);
-    snprintf(str1, 23, "i = %.1f", i);
-    ST7735_WriteString(0, 36, str1, Font_11x18, ST7735_WHITE, ST7735_BLACK);
+    // ST7735_WriteString(0, 0, "FOC", Font_11x18, ST7735_RED, ST7735_BLACK);
+    // // ST7735_WriteString(40, 0, "MODE1", Font_11x18, ST7735_RED, ST7735_BLACK);
+    // // ST7735_WriteString(0, 18, "U=5.68V", Font_11x18, ST7735_GREEN, ST7735_BLACK);
+    // // ST7735_WriteString(0, 38, "V=6.23V", Font_11x18, ST7735_BLUE, ST7735_BLACK);
+    // // ST7735_WriteString(0, 60, "W=3.26V", Font_11x18, ST7735_YELLOW, ST7735_BLACK);
+    // snprintf(str1, 23, "i = %.1f", i);
+
     i = i + 0.3f;
-    HAL_Delay(10);
+    ST7735_DrawString(0, 0, "Hello World 123456789", ST7735_RED, ST7735_BLACK, &Font_11x18);
+    ST7735_DrawString(0, 40, str1, ST7735_YELLOW, ST7735_BLACK, &Font_11x18);
+
+    // HAL_Delay(10);
+    // ST7735_FillScreen(ST7735_RED);
+    // HAL_Delay(500);
+    // ST7735_FillScreen(ST7735_GREEN);
+    // HAL_Delay(500);
+    // ST7735_FillScreen(ST7735_BLUE);
+    // HAL_Delay(500);
+    // ST7735_DrawString(0, 0, "Hello", ST7735_BLUE, ST7735_BLACK, &Font_11x18);
 }
--- 1.ST7735/README.md
+++ 1.ST7735/README.md
@@ -1,4 +1,8 @@
-源码参考：https://github.com/afiskon/stm32-st7735
+参考资料：
+
+> 1.https://github.com/afiskon/stm32-st7735/blob/master/st7735/st7735.c    
+>
+> 2.https://blog.csdn.net/Xiaowu12345678910/article/details/129038133
 
 0.96寸LCD的尺寸偏移：
 
@@ -19,3 +23,55 @@
  */
 void ST7735_SetRotation(uint8_t rotation)
 ```
+
+# 测试程序
+
+屏幕颜色测试：
+
+```c
+    ST7735_FillScreen(ST7735_RED);
+    HAL_Delay(500);
+    ST7735_FillScreen(ST7735_GREEN);
+    HAL_Delay(500);
+    ST7735_FillScreen(ST7735_BLUE);
+    HAL_Delay(500);
+```
+
+显示字符串：
+
+```c
+ ST7735_DrawString(0, 0, "Hello World", ST7735_RED, ST7735_BLACK, &Font_11x18);
+```
+
+显示变量：
+
+```c
+    static float i = 0.0f;
+    snprintf(str1, 23, "i = %.1f", i);
+    i = i + 0.3f;
+    ST7735_DrawString(0, 0, "Hello World", ST7735_RED, ST7735_BLACK, &Font_11x18);
+    ST7735_DrawString(0, 20, str1, ST7735_YELLOW, ST7735_BLACK, &Font_11x18);
+```
+
+显示变量的工程，程序编译大小：
+
+```c
+    Total RO  Size (Code + RO Data)                28036 (  27.38kB)
+    Total RW  Size (RW Data + ZI Data)              2456 (   2.40kB)
+    Total ROM Size (Code + RO Data + RW Data)      28056 (  27.40kB)
+```
+
+- **Flash (ROM):** Total ROM Size (Code + RO Data + RW Data) 是 28056 字节，或者27.40KB。这表示程序在Flash（只读存储器）中占用了27.40KB的空间。
+- **RAM:** Total RW Size (RW Data + ZI Data) 是 2456 字节，或者2.40KB。这表示程序在RAM（随机存储器）中占用了2.40KB的空间。
+
+## 程序内存分配
+
+- Code：编译后的程序机器代码，占用了只读存储器(ROM)的一部分。包括程序的执行指令，用于实现程序的逻辑和功能。
+- RO Data：只读数据(只读存储器，如Flash存储器，有助于减少RAM的使用，并提高程序的运行效率)，在程序运行时不会被修改的数据：常量、只读变量
+- RW Data：读写数据，指程序在运行时使用的数据：全局变量、静态变量、以及在程序运行时动态分配的内存。
+- ZI Data：零初始化数据，在程序启动时被初始化为零值的数据。
+
+**RAM = RW Data + ZI Data**
+
+**Flash(ROM) = Code + RO Data + RW Data**
+
